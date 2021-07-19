98d60ce96fcf5b3ed397b5599ef89d339d57e7a2
Lei Xu <komushi@gmail.com>
2021-07-19 18:55:40 +0900

    deviceReg

--- src/api/http.js
+++ src/api/http.js
@@ -77,7 +77,7 @@ router.post('/deviceReg', async (req, res) => {
   const publishResults = await Promise.all(newScanners.Items.map(async(item) => {
       return new Promise((resolve, reject) => {
         iotData.publish({
-          topic: `${AWS_IOT_THING_NAME}/update_scanner`,
+          topic: `${process.env.AWS_IOT_THING_NAME}/update_scanner`,
           payload: JSON.stringify(item)
         }, (err, data) =>{
           if (err) {
