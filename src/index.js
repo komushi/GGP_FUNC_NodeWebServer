80f6c425bc813df24949fe67a1c8a72cda543799
Lei Xu <komushi@gmail.com>
2021-09-07 23:48:39 +0900

    express size

--- src/index.js
+++ src/index.js
@@ -66,8 +66,8 @@ const initialize = () => {
 
     const app = express();
 
-    app.use(express.json()); 
-    app.use(express.urlencoded({extended: true}));
+    app.use(express.json({limit: '50mb'})); 
+    app.use(express.urlencoded({limit: '50mb', extended: true}));
     routerHandler(app);
 
 
